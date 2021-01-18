5a8f8b23214e679811472336e28aed63b9d09dd4
Konstantin Naryshkin <2186793+kontaras@users.noreply.github.com>
2021-01-17 22:01:26 -0500

    Initial push of gradle structure

---
+++ src/main/java/kon/legendarystatsserver/LegendaryStatsServerApplication.java
@@ -0,0 +1,13 @@
+package kon.legendarystatsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegendaryStatsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegendaryStatsServerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/kon/legendarystatsserver/model/game/Set.java
@@ -0,0 +1,62 @@
+package kon.legendarystatsserver.model.game;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * A set of the game.
+ */
+@Entity
+public class Set
+{
+	/** Database ID */
+	@Id
+	private Integer id;
+	
+	/** The name of the set for Marvel comics sets */
+	private String marvelName;
+	/** The name of the set for Marvel movie stills sets */
+	private String mcuName;
+	/** The name of the set for Legendary DXP sets */
+	private String dxpName;
+	
+	public Integer getId()
+	{
+		return this.id;
+	}
+
+	public void setId(Integer id)
+	{
+		this.id = id;
+	}
+
+	public String getMarvelName()
+	{
+		return this.marvelName;
+	}
+
+	public void setMarvelName(String marvelName)
+	{
+		this.marvelName = marvelName;
+	}
+
+	public String getMcuName()
+	{
+		return this.mcuName;
+	}
+
+	public void setMcuName(String mcuName)
+	{
+		this.mcuName = mcuName;
+	}
+
+	public String getDxpName()
+	{
+		return this.dxpName;
+	}
+
+	public void setDxpName(String dxpName)
+	{
+		this.dxpName = dxpName;
+	}
+}
