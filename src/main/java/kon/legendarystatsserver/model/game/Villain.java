d85b6278931b423cbfbeea1babe9a64dabc3fbd4
Konstantin Tarashchanskiy <knaryshkin@vmware.com>
2021-01-18 22:06:33 -0500

    Added villain table

---
+++ src/main/java/kon/legendarystatsserver/model/game/Villain.java
@@ -0,0 +1,61 @@
+package kon.legendarystatsserver.model.game;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Villain {
+	/** Database ID */
+	@Id
+	private Integer id;
+
+	/** The name of the set for Marvel comics sets */
+	private String marvelName;
+	
+	/** The name of the set for Marvel movie stills sets */
+	private String mcuName;
+	
+	/** The name of the set for Legendary DXP sets */
+	private String dxpName;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Set set;
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @return the marvelName
+	 */
+	public String getMarvelName() {
+		return marvelName;
+	}
+
+	/**
+	 * @return the mcuName
+	 */
+	public String getMcuName() {
+		return mcuName;
+	}
+
+	/**
+	 * @return the dxpName
+	 */
+	public String getDxpName() {
+		return dxpName;
+	}
+
+	/**
+	 * @return the set
+	 */
+	public Set getSet() {
+		return set;
+	}
+
+}
--- src/main/resources/db/migration/V2__base_set.sql
+++ src/main/resources/db/migration/V2__base_set.sql
@@ -2,5 +2,12 @@ INSERT INTO set (id, marvel_name, dxp_name, mcu_name) VALUES (1, 'Base Set', 'Ba
 
 INSERT INTO hero (set_id, id, marvel_name, dxp_name, mcu_name) VALUES (1, 101, 'Black Widow', 'Steelvara the Light', 'Black Widow');
 INSERT INTO hero (set_id, id, marvel_name, dxp_name, mcu_name) VALUES (1, 102, 'Captain America', 'Lord Cedric of the Citadel', 'Captain America');
-
 --'"Hog" Dryll'
+
+INSERT INTO mastermind (set_id, id, marvel_name, dxp_name, mcu_name) VALUES (1, 101, 'Dr. Doom', 'Durissa the Dispossessed', null);
+INSERT INTO mastermind (set_id, id, marvel_name, dxp_name, mcu_name) VALUES (1, 102, null, 'Epic Durissa the Dispossessed', null);
+INSERT INTO mastermind (set_id, id, marvel_name, dxp_name, mcu_name) VALUES (1, 199, null, null, 'Iron Monger');
+
+INSERT INTO henchman (set_id, id, marvel_name, dxp_name, mcu_name) VALUES (1, 199, 'Doombot Legion', 'Lord Cedric of the Citadel', null);
+
+INSERT INTO villain (set_id, id, marvel_name, dxp_name, mcu_name) VALUES (1, 199, null, null, 'Iron Foes');
