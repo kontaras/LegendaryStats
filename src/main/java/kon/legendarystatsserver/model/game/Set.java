9f4aa5918f51553211da110c69da7d1d4e177b06
Konstantin Tarashchanskiy <knaryshkin@vmware.com>
2021-01-18 19:54:34 -0500

    Added set table

--- src/main/java/kon/legendarystatsserver/model/game/Set.java
+++ src/main/java/kon/legendarystatsserver/model/game/Set.java
@@ -7,56 +7,47 @@ import javax.persistence.Id;
  * A set of the game.
  */
 @Entity
-public class Set
-{
+public class Set {
 	/** Database ID */
 	@Id
 	private Integer id;
-	
+
 	/** The name of the set for Marvel comics sets */
 	private String marvelName;
 	/** The name of the set for Marvel movie stills sets */
 	private String mcuName;
 	/** The name of the set for Legendary DXP sets */
 	private String dxpName;
-	
-	public Integer getId()
-	{
+
+	public Integer getId() {
 		return this.id;
 	}
 
-	public void setId(Integer id)
-	{
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
-	public String getMarvelName()
-	{
+	public String getMarvelName() {
 		return this.marvelName;
 	}
 
-	public void setMarvelName(String marvelName)
-	{
+	public void setMarvelName(String marvelName) {
 		this.marvelName = marvelName;
 	}
 
-	public String getMcuName()
-	{
+	public String getMcuName() {
 		return this.mcuName;
 	}
 
-	public void setMcuName(String mcuName)
-	{
+	public void setMcuName(String mcuName) {
 		this.mcuName = mcuName;
 	}
 
-	public String getDxpName()
-	{
+	public String getDxpName() {
 		return this.dxpName;
 	}
 
-	public void setDxpName(String dxpName)
-	{
+	public void setDxpName(String dxpName) {
 		this.dxpName = dxpName;
 	}
 }
