3a816c60801fc0922dab4703833624bfa260b2c4
Konstantin Tarashchanskiy <knaryshkin@vmware.com>
2021-01-18 22:10:32 -0500

    Moved repositories into seperate package

--- src/main/java/kon/legendarystatsserver/api/DataController.java
+++ src/main/java/kon/legendarystatsserver/api/DataController.java
@@ -7,9 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import kon.legendarystatsserver.model.game.SetsRepository;
 import kon.legendarystatsserver.model.game.Villain;
-import kon.legendarystatsserver.model.game.VillainsRepository;
+import kon.legendarystatsserver.model.game.repositories.SetsRepository;
+import kon.legendarystatsserver.model.game.repositories.VillainsRepository;
 
 /**
  * @author knaryshkin
