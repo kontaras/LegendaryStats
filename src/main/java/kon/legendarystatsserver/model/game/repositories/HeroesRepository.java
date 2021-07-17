da6bfbaecab7da16cab955255154afa5f6542d8e
Konstantin Tarashchanskiy <2186793+kontaras@users.noreply.github.com>
2021-07-17 00:15:59 -0400

    Removed alignment spaces from SQL query

--- src/main/java/kon/legendarystatsserver/model/game/repositories/HeroesRepository.java
+++ src/main/java/kon/legendarystatsserver/model/game/repositories/HeroesRepository.java
@@ -10,12 +10,12 @@ import kon.legendarystatsserver.model.game.Hero;
 public interface HeroesRepository extends ReadOnlyRepository<Hero, Integer> {
 
 	@Query(value = "SELECT h.id AS id, "
-			+ "			COUNT(*) AS played,"
-			+ "			COUNT(CASE WHEN p.outcome = 'win' THEN 1 ELSE null END) AS won, "
-			+ "			COUNT(CASE WHEN p.outcome = 'loss' THEN 1 ELSE null END) AS lost "
-			+ "		FROM Hero h INNER JOIN h.plays p"
-			+ "		GROUP BY h"
-			+ "		ORDER BY COUNT(CASE WHEN p.outcome = 'win' THEN 1 ELSE null END) / "
-			+ "				 CAST(COUNT(*) AS float)")
+			+ "	        COUNT(*) AS played,"
+			+ "	        COUNT(CASE WHEN p.outcome = 'win' THEN 1 ELSE null END) AS won, "
+			+ "	        COUNT(CASE WHEN p.outcome = 'loss' THEN 1 ELSE null END) AS lost "
+			+ "    FROM Hero h INNER JOIN h.plays p"
+			+ "    GROUP BY h"
+			+ "    ORDER BY COUNT(CASE WHEN p.outcome = 'win' THEN 1 ELSE null END) / "
+			+ "             CAST(COUNT(*) AS float)")
 	List<IWinRate> findHeroWinRates();
 }
