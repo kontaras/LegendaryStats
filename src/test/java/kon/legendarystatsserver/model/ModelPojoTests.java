c665a27d2688cde0f81444c2cbf85e91cc9ddac2
Konstantin Tarashchanskiy <2186793+kontaras@users.noreply.github.com>
2021-07-17 00:09:32 -0400

    src/test/java/kon/legendarystatsserver/test/util/PojoTest.java -> src/test/java/kon/legendarystatsserver/test/util/PojoTestUtil.java

--- src/test/java/kon/legendarystatsserver/model/ModelPojoTests.java
+++ src/test/java/kon/legendarystatsserver/model/ModelPojoTests.java
@@ -2,12 +2,12 @@ package kon.legendarystatsserver.model;
 
 import org.junit.jupiter.api.Test;
 
-import kon.legendarystatsserver.test.util.PojoTest;
+import kon.legendarystatsserver.test.util.PojoTestUtil;
 
 /**
  * Test POJOs in the {@link kon.legendarystatsserver.model} package for coverage and to weed out stupid bugs.
  */
-public class ModelPojoTests extends PojoTest {
+public class ModelPojoTests extends PojoTestUtil {
 
 	/**
 	 * Test {@link Play}
