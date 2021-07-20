5baf10f5cb7c5d85cd8cdeefae0648e4de98b354
Konstantin Tarashchanskiy <2186793+kontaras@users.noreply.github.com>
2021-07-20 00:48:18 -0400

    Added name filtering

---
+++ src/main/resources/static/scripts/utils.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+
+
+function setNames (table, cls) {
+	let cells = table.find("td.name").children().filter("span." + cls)
+	
+	cells.css("display", "inline");
+	cells.siblings().css("display", "none");
+}
