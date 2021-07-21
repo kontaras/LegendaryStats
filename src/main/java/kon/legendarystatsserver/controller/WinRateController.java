7ea782acc0681a8ab8417d22ba8a8d859f87e03b
Konstantin Tarashchanskiy <2186793+kontaras@users.noreply.github.com>
2021-07-21 15:42:23 -0400

    Revert "Renamed Model-View-Controller Controllers to be Services to better match Spring Web terminology"
    This reverts commit ed3986e0bb1ee45f9fa6304a0f9a86d21c244547.

---
+++ src/main/java/kon/legendarystatsserver/controller/WinRateController.java
@@ -0,0 +1,49 @@
+package kon.legendarystatsserver.controller;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.tinylog.Logger;
+
+import kon.legendarystatsserver.model.game.Hero;
+import kon.legendarystatsserver.model.game.repositories.HeroesRepository;
+import kon.legendarystatsserver.model.game.repositories.IWinRate;
+
+/**
+ * Controller to get the win rates of each card set.
+ */
+@Controller
+public class WinRateController {
+	
+	/**
+	 * Repository that can get us hero win rates
+	 */
+	@Autowired
+	private HeroesRepository heroes;
+	
+	/**
+	 * Directory that we use to dereference IDs to actual objects
+	 */
+	@Autowired
+	private CardDirectory directory;
+
+	/**
+	 * Get all qualifying heroes with their {@link IWinRate}.
+	 * 
+	 * @return The list of heroes, ordered by the win percentage of each hero.
+	 */
+	public Map<Hero, IWinRate> getHeroWinRates() {
+		Logger.info("Starting to get hero win rates");
+		long start = System.currentTimeMillis();
+		List<IWinRate> winRates = heroes.findHeroWinRates();
+		Logger.info("Finished getting win rates, took {}ms", ()-> (System.currentTimeMillis() - start));
+		Map<Hero, IWinRate> winRateMap = new LinkedHashMap<>();
+		for (IWinRate winRate : winRates) {
+			Hero hero = directory.getHeroById(winRate.getId());
+			winRateMap.put(hero, winRate);
+		}
+		return winRateMap;
+	}
+}
--- src/main/java/kon/legendarystatsserver/view/FrontPageController.java
+++ src/main/java/kon/legendarystatsserver/view/FrontPageController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import kon.legendarystatsserver.service.WinRateService;
+import kon.legendarystatsserver.controller.WinRateController;
 
 /**
  * A controller for the index page.
@@ -13,7 +13,7 @@ import kon.legendarystatsserver.service.WinRateService;
 @Controller
 public class FrontPageController {
 	@Autowired
-	private WinRateService winRates;
+	private WinRateController winRates;
 	
 	@GetMapping("/")
 	public String mainPage(Model model) {
