530583923e5fc7cf916b9660e33584fd982465f2
Konstantin Tarashchanskiy <knary@users.noreply.github.com>
2021-07-21 15:48:48 -0400

    Renamed Model-View-Controller Controllers to be Services to better match Spring Web terminology part 2: file names

--- src/main/java/kon/legendarystatsserver/view/FrontPageController.java
+++ src/main/java/kon/legendarystatsserver/view/FrontPageController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import kon.legendarystatsserver.service.WinRateController;
+import kon.legendarystatsserver.service.WinRateService;
 
 /**
  * A controller for the index page.
@@ -13,7 +13,7 @@ import kon.legendarystatsserver.service.WinRateController;
 @Controller
 public class FrontPageController {
 	@Autowired
-	private WinRateController winRates;
+	private WinRateService winRates;
 	
 	@GetMapping("/")
 	public String mainPage(Model model) {
