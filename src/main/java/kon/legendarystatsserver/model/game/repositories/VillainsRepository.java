6c1940d48f98488a0a9c2760167eec6495ba6484
Konstantin Tarashchanskiy <knary@users.noreply.github.com>
2021-07-25 23:35:39 -0400

    Added villain win rate table.

--- src/main/java/kon/legendarystatsserver/model/game/repositories/VillainsRepository.java
+++ src/main/java/kon/legendarystatsserver/model/game/repositories/VillainsRepository.java
@@ -1,5 +1,8 @@
 package kon.legendarystatsserver.model.game.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import kon.legendarystatsserver.model.game.Villain;
@@ -7,4 +10,13 @@ import kon.legendarystatsserver.model.game.Villain;
 @RepositoryRestResource
 public interface VillainsRepository extends CardSetRepository<Villain, Integer> {
 
+	@Query(value = "SELECT v.id AS id, "
+			+ "         COUNT(*) AS played,"
+			+ "         COUNT(CASE WHEN p.outcome = 'win' THEN 1 ELSE null END) AS won, "
+			+ "         COUNT(CASE WHEN p.outcome = 'loss' THEN 1 ELSE null END) AS lost "
+			+ "    FROM Villain v INNER JOIN v.plays p"
+			+ "    GROUP BY v"
+			+ "    ORDER BY COUNT(CASE WHEN p.outcome = 'win' THEN 1 ELSE null END) / "
+			+ "             CAST(COUNT(*) AS float) DESC")
+	List<IWinRate> findVillainWinRates();
 }
