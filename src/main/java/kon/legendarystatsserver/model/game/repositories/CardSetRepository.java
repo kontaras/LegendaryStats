f1c6594329d980ba2b8a99110f4c755a90e0dd26
Konstantin Tarashchanskiy <2186793+kontaras@users.noreply.github.com>
2021-07-26 21:39:51 -0400

    Refactored HeroesRepository and VillainsRepository to push up shared logic into a common parent class.

---
+++ src/main/java/kon/legendarystatsserver/model/game/repositories/CardSetRepository.java
@@ -0,0 +1,24 @@
+package kon.legendarystatsserver.model.game.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import kon.legendarystatsserver.model.game.CardSet;
+
+/**
+ * Common repository methods to be used for all {@link CardSet}s
+ */
+@NoRepositoryBean
+public interface CardSetRepository<T extends CardSet, ID> extends ReadOnlyRepository<T, ID> {
+	@Query(value = "SELECT h.id AS id, "
+			+ "         COUNT(*) AS played,"
+			+ "         COUNT(CASE WHEN p.outcome = 'win' THEN 1 ELSE null END) AS won, "
+			+ "         COUNT(CASE WHEN p.outcome = 'loss' THEN 1 ELSE null END) AS lost "
+			+ "    FROM #{#entityName} h INNER JOIN h.plays p"
+			+ "    GROUP BY h"
+			+ "    ORDER BY COUNT(CASE WHEN p.outcome = 'win' THEN 1 ELSE null END) / "
+			+ "             CAST(COUNT(*) AS float) DESC")
+	List<IWinRate> findWinRates();
+}
