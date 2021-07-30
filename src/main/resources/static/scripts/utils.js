5197a1cd466bd045b486596376df004639b287f1
Konstantin Tarashchanskiy <knary@users.noreply.github.com>
2021-07-30 13:20:20 -0400

    Issue #33: Use Thymeleaf to reduce boilerplate in table generation

--- src/main/resources/static/scripts/utils.js
+++ src/main/resources/static/scripts/utils.js
@@ -2,9 +2,14 @@
  * 
  */
 
-function setNames (table, cls) {
-	let cells = table.find("td.name").children().filter("span." + cls)
-	
-	cells.css("display", "inline");
-	cells.siblings().css("display", "none");
+window.tableIds = []
+
+function setNames (cls) {
+	for (const tableId of window.tableIds) {
+		const table = jQuery("table#" + tableId)
+		const cells = table.find("td.name").children().filter("span." + cls)
+		
+		cells.css("display", "inline");
+		cells.siblings().css("display", "none");
+	}
 }
