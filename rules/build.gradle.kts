0fd3ff395447f4753b9dadacb00c142b55102cc8
Konstantin Tarashchanskiy <2186793+kontaras@users.noreply.github.com>
2021-08-03 22:41:11 -0400

    Initial Kotlin project shape

---
+++ rules/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    kotlin("multiplatform") version "1.5.10"
+}
+
+group = "kon.legendarystats.rules"
+version = "0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "11"
+        }
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+    js(LEGACY) {
+        browser {
+            commonWebpackConfig {
+                cssSupport.enabled = true
+            }
+        }
+    }
+
+    
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val jvmMain by getting
+        val jvmTest by getting
+        val jsMain by getting
+        val jsTest by getting
+    }
+}
