c4d81d57189a63c38cd52cc7a3e5fb17608d979f
Konstantin Tarashchanskiy <2186793+kontaras@users.noreply.github.com>
2021-08-06 11:38:25 -0400

    Add basic data model, put in rules about sets per player count

---
+++ rules/src/commonMain/kotlin/verifier/Verifier.kt
@@ -0,0 +1,39 @@
+package kon.foo.verifier
+
+import kon.foo.model.Play
+import kon.foo.model.PlayerCount
+
+fun verify(play: Play): List<PrintableError>{
+    var errors = mutableListOf<PrintableError>()
+    val setCounts = getPlayerCountRules(play.players)
+
+    if (play.heroes.size != setCounts.heroes) {
+        errors.add(WrongHeroCount(setCounts.heroes, play.heroes.size))
+    }
+
+    return errors
+}
+
+public fun getPlayerCountRules(playerCount: PlayerCount): SetCounts {
+    return when(playerCount) {
+        PlayerCount.SOLO -> SetCounts(3, 1, 1)
+        PlayerCount.ADVANCED -> SetCounts(3, 1, 1)
+        PlayerCount.TWO -> SetCounts(5,2,1)
+        PlayerCount.THREE -> SetCounts(5,3,1)
+        PlayerCount.FOUR -> SetCounts(5,3,2)
+        PlayerCount.FIVE -> SetCounts(6,4,2)
+    }
+}
+
+data class SetCounts(var heroes: Int, var villains: Int, var henchmen: Int)
+
+public interface PrintableError {
+    fun getMessage(): String
+}
+
+class WrongHeroCount(val expected: Int, val actual: Int) : PrintableError {
+    override fun getMessage(): String {
+        return "Expected to provide $expected heroes, got $actual"
+    }
+
+}
