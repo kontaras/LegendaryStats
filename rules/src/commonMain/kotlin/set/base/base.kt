1749fe4b9b276cafdf8768f890b8c5eea9b04c7b
Konstantin Tarashchanskiy <2186793+kontaras@users.noreply.github.com>
2021-08-06 16:09:32 -0400

    Added tests for verifier to test the size of heroes list

---
+++ rules/src/commonMain/kotlin/set/base/base.kt
@@ -0,0 +1,31 @@
+package kon.foo.set.base
+
+public const val BASE_SET = 1
+
+public object Heroes {
+    const val BLACK_WIDOW = 101
+    const val CAPTAIN_AMERICA = 102
+    const val CYCLOPS = 103
+    const val DEADPOOL = 104
+
+    const val GAMBIT = 106
+
+    const val IRON_MAN = 110
+}
+
+public object Schemes {
+    const val THE_LEGACY_VIRUS = 101
+}
+
+public object Masterminds {
+    const val LOKI = 103
+}
+
+public object Villains {
+    const val BROTHERHOOD = 101
+    const val ENEMIES_OF_ASGARD = 102
+}
+
+public object Henchmen {
+    const val SENTINEL = 104
+}
