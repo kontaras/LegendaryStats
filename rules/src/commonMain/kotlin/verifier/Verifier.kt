01c230a13bda366553fd8018a4d345940baef97c
Konstantin Tarashchanskiy <knary@users.noreply.github.com>
2021-08-06 23:16:57 -0400

    Add verification of villian and henchman set counts

--- rules/src/commonMain/kotlin/verifier/Verifier.kt
+++ rules/src/commonMain/kotlin/verifier/Verifier.kt
@@ -3,45 +3,41 @@ package kon.foo.verifier
 import kon.foo.model.Play
 import kon.foo.model.PlayerCount
 
-fun verify(play: Play): List<PrintableError>{
-    var errors = mutableListOf<PrintableError>()
+fun verify(play: Play): List<PrintableError> {
+    val errors = mutableListOf<PrintableError>()
     val setCounts = getPlayerCountRules(play.players)
 
+    errors.addAll(checkSetSizes(play, setCounts))
+
+    return errors
+}
+
+private fun checkSetSizes(play: Play, setCounts: SetCounts,): MutableList<PrintableError> {
+    val errors = mutableListOf<PrintableError>()
     if (play.heroes.size != setCounts.heroes) {
-        errors.add(WrongHeroCount(setCounts.heroes, play.heroes.size))
+        errors.add(WrongSetCount("hero", setCounts.heroes, play.heroes.size))
+    }
+
+    if (play.villains.size != setCounts.villains) {
+        errors.add(WrongSetCount("villain", setCounts.villains, play.villains.size))
+    }
+
+    if (play.henchmen.size != setCounts.henchmen) {
+        errors.add(WrongSetCount("henchman", setCounts.henchmen, play.henchmen.size))
     }
 
     return errors
 }
 
-public fun getPlayerCountRules(playerCount: PlayerCount): SetCounts {
-    return when(playerCount) {
+fun getPlayerCountRules(playerCount: PlayerCount): SetCounts {
+    return when (playerCount) {
         PlayerCount.SOLO -> SetCounts(3, 1, 1)
         PlayerCount.ADVANCED -> SetCounts(3, 1, 1)
-        PlayerCount.TWO -> SetCounts(5,2,1)
-        PlayerCount.THREE -> SetCounts(5,3,1)
-        PlayerCount.FOUR -> SetCounts(5,3,2)
-        PlayerCount.FIVE -> SetCounts(6,4,2)
+        PlayerCount.TWO -> SetCounts(5, 2, 1)
+        PlayerCount.THREE -> SetCounts(5, 3, 1)
+        PlayerCount.FOUR -> SetCounts(5, 3, 2)
+        PlayerCount.FIVE -> SetCounts(6, 4, 2)
     }
 }
 
 data class SetCounts(var heroes: Int, var villains: Int, var henchmen: Int)
-
-public interface PrintableError {
-    fun getMessage(): String
-}
-
-class WrongHeroCount(val expected: Int, val actual: Int) : PrintableError {
-    override fun getMessage(): String {
-        return "Expected to provide $expected heroes, got $actual"
-    }
-
-    override fun equals(other: Any?): Boolean {
-        return (other is WrongHeroCount) && this.actual == other.actual && this.expected == other.expected
-    }
-
-    override fun hashCode(): Int {
-        val largeEnoughPrime = 13
-        return actual * largeEnoughPrime + expected
-    }
-}
--- rules/src/commonTest/kotlin/verifier/VerifierKtTest.kt
+++ rules/src/commonTest/kotlin/verifier/VerifierKtTest.kt
@@ -8,12 +8,9 @@ import kon.foo.set.base.Masterminds
 import kon.foo.model.Outcome
 import kon.foo.model.Play
 import kon.foo.model.PlayerCount
-import kon.foo.verifier.SetCounts
-import kon.foo.verifier.WrongHeroCount
-import kon.foo.verifier.getPlayerCountRules
-import kon.foo.verifier.verify
 import kotlin.test.assertEquals
 import kotlin.test.Test
+import kotlin.test.assertContentEquals
 
 internal class VerifierKtTest {
 
@@ -21,25 +18,42 @@ internal class VerifierKtTest {
     fun verifyCardSetCounts() {
         val heroes: MutableSet<Int> =
             mutableSetOf(Heroes.BLACK_WIDOW, Heroes.CAPTAIN_AMERICA, Heroes.CYCLOPS, Heroes.IRON_MAN, Heroes.GAMBIT)
-        val villains: MutableSet<Int> = mutableSetOf(Villains.ENEMIES_OF_ASGARD, Villains.BROTHERHOOD)
-        val henchmen: MutableSet<Int> = mutableSetOf(Henchmen.SENTINEL)
+        val villains: MutableSet<Int> = mutableSetOf(Villains.ENEMIES_OF_ASGARD, Villains.BROTHERHOOD, Villains.HYDRA)
+        val henchmen: MutableSet<Int> = mutableSetOf(Henchmen.SENTINEL, Henchmen.DOOMBOT_LEGION)
         val testPlay = Play(
             Outcome.DRAW,
-            PlayerCount.TWO,
+            PlayerCount.FOUR,
             Schemes.THE_LEGACY_VIRUS,
             Masterminds.LOKI,
             heroes,
             villains,
             henchmen,
             null
-        );
+        )
 
         heroes.remove(Heroes.GAMBIT)
-        assertEquals(listOf(WrongHeroCount(5, 4)), verify(testPlay))
-
+        assertContentEquals(listOf(WrongSetCount("hero", 5, 4)), verify(testPlay))
         heroes.add(Heroes.GAMBIT)
+
         heroes.add(Heroes.DEADPOOL)
-        assertEquals(listOf(WrongHeroCount(5, 6)), verify(testPlay))
+        assertContentEquals(listOf(WrongSetCount("hero", 5, 6)), verify(testPlay))
+        heroes.remove(Heroes.GAMBIT)
+
+        villains.remove(Villains.ENEMIES_OF_ASGARD)
+        assertContentEquals(listOf(WrongSetCount("villain", 3, 2)), verify(testPlay))
+        villains.add(Villains.ENEMIES_OF_ASGARD)
+
+        villains.add(Villains.MASTERS_OF_EVIL)
+        assertContentEquals(listOf(WrongSetCount("villain", 3, 4)), verify(testPlay))
+        villains.remove(Villains.ENEMIES_OF_ASGARD)
+
+        henchmen.remove(Henchmen.DOOMBOT_LEGION)
+        assertContentEquals(listOf(WrongSetCount("henchman", 2, 1)), verify(testPlay))
+        henchmen.add(Henchmen.DOOMBOT_LEGION)
+
+        henchmen.add(Henchmen.HAND_NINJAS)
+        assertContentEquals(listOf(WrongSetCount("henchman", 2, 3)), verify(testPlay))
+        henchmen.remove(Henchmen.DOOMBOT_LEGION)
     }
 
     @Test
