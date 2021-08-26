12ba1a983c9d1927bb085c3882bb974265da3eae
Konstantin Tarashchanskiy <2186793+kontaras@users.noreply.github.com>
2021-08-26 18:27:09 -0400

    Change capitalization to work with PostgeSQL, unified capitalization

--- src/main/resources/db/migration/V1_1__base_set.sql
+++ src/main/resources/db/migration/V1_1__base_set.sql
@@ -3,19 +3,19 @@ INSERT INTO release (id, marvel_name, dxp_name, mcu_name) VALUES (1, 'Base Set',
 INSERT INTO hero (release_id, id, marvel_name, dxp_name, mcu_name) VALUES
 	(1, 101, 'Black Widow', 'Steelvara the Light', 'Black Widow'),
 	(1, 102, 'Captain America', 'Lord Cedric of the Citadel', 'Captain America'),
-	(1, 103, 'Cyclops', 'Disaray the Sufferer', null),
-	(1, 104, 'Deadpool', 'Nunchi', null),
-	(1, 105, 'Emma Frost', 'Doneya Petalfall', null),
-	(1, 106, 'Gambit', 'Makea the All-Knowing', null),
+	(1, 103, 'Cyclops', 'Disaray the Sufferer', NULL),
+	(1, 104, 'Deadpool', 'Nunchi', NULL),
+	(1, 105, 'Emma Frost', 'Doneya Petalfall', NULL),
+	(1, 106, 'Gambit', 'Makea the All-Knowing', NULL),
 	(1, 107, 'Hawkeye', 'Arcillo the Noble Hearted', 'Hawkeye'),
 	(1, 108, 'Hulk', 'Cawr', 'Hulk'),
 	(1, 109, 'Iron Man', 'Lollycooler', 'Iron Man'),
 	(1, 110, 'Nick Fury', 'Mal Gravemore', 'Nick Fury'),
-	(1, 111, 'Rogue', 'Ceridwen', null),
-	(1, 112, 'Spider-Man', 'Ryuhi', null),
-	(1, 113, 'Storm', 'Ordmantil the Shadow', null),
+	(1, 111, 'Rogue', 'Ceridwen', NULL),
+	(1, 112, 'Spider-Man', 'Ryuhi', NULL),
+	(1, 113, 'Storm', 'Ordmantil the Shadow', NULL),
 	(1, 114, 'Thor', '"Hog" Dryll', 'Thor'),
-	(1, 115, 'Wolverine', 'Kamina the Curious', null);
+	(1, 115, 'Wolverine', 'Kamina the Curious', NULL);
 
 
 INSERT INTO scheme (release_id, id, marvel_name, dxp_name, mcu_name) VALUES
@@ -29,27 +29,27 @@ INSERT INTO scheme (release_id, id, marvel_name, dxp_name, mcu_name) VALUES
 	(1, 108, 'Unleash the Power of the Cosmic Cube', 'Apex of Power', 'Unleash the Power of the Cosmic Cube');
 
 INSERT INTO mastermind (release_id, id, marvel_name, dxp_name, mcu_name) VALUES
-	(1, 101, 'Dr. Doom', 'Durissa the Dispossessed', null),
-	(1, 102, null, 'Epic Durissa the Dispossessed', null),
+	(1, 101, 'Dr. Doom', 'Durissa the Dispossessed', NULL),
+	(1, 102, NULL, 'Epic Durissa the Dispossessed', NULL),
 	(1, 103, 'Loki', 'Terriskai, Terror of the Skies', 'Loki'),
-	(1, 104, null, 'Epic Terriskai, Terror of the Skies', null),
-	(1, 105, 'Magneto', 'Nax, Lord of Crimson Bog', null),
-	(1, 106, null, 'Epic Nax, Lord of Crimson Bog', null),
+	(1, 104, NULL, 'Epic Terriskai, Terror of the Skies', NULL),
+	(1, 105, 'Magneto', 'Nax, Lord of Crimson Bog', NULL),
+	(1, 106, NULL, 'Epic Nax, Lord of Crimson Bog', NULL),
 	(1, 107, 'Red Skull', 'Kelila, Bender of Wills', 'Red Skull'),
-	(1, 108, null, 'Epic Kelila, Bender of Wills', null),
-	(1, 109, null, null, 'Iron Monger');
+	(1, 108, NULL, 'Epic Kelila, Bender of Wills', NULL),
+	(1, 109, NULL, NULL, 'Iron Monger');
 
 INSERT INTO villain (release_id, id, marvel_name, dxp_name, mcu_name) VALUES
-	(1, 101, 'Brotherhood', 'Sarco', null),
+	(1, 101, 'Brotherhood', 'Sarco', NULL),
 	(1, 102, 'Enemies of Asgard', 'Nortagem', 'Enemies of Asgard'),
-	(1, 103, 'HYDRA', 'Ernak the Lethal', null),
+	(1, 103, 'HYDRA', 'Ernak the Lethal', NULL),
 	(1, 104, 'Masters of Evil', 'Void Rider', 'HYDRA'),
-	(1, 105, 'Radiation', 'Force of Gehenna', null),
-	(1, 106, 'Skrulls', 'Brigitte Blackbird', null),
-	(1, 107, 'Spider-Foes', 'Teuthos', null),
-	(1, 108, null, null, 'Chitauri'),
-	(1, 109, null, null, 'Gamma Hunters'),
-	(1, 110, null, null, 'Iron Foes');
+	(1, 105, 'Radiation', 'Force of Gehenna', NULL),
+	(1, 106, 'Skrulls', 'Brigitte Blackbird', NULL),
+	(1, 107, 'Spider-Foes', 'Teuthos', NULL),
+	(1, 108, NULL, NULL, 'Chitauri'),
+	(1, 109, NULL, NULL, 'Gamma Hunters'),
+	(1, 110, NULL, NULL, 'Iron Foes');
 
 INSERT INTO henchman (release_id, id, marvel_name, dxp_name, mcu_name) VALUES
 	(1, 101, 'Doombot Legion', 'Goblin Horde', 'Hammer Drone Army'),
