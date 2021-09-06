9496177f80e3c73fea393f516cd6b5368bb29468
Konstantin Tarashchanskiy <2186793+kontaras@users.noreply.github.com>
2021-09-05 22:40:14 -0400

    Generate plays and heroes

--- utils_py/db_gen.py
+++ utils_py/db_gen.py
@@ -1,5 +1,6 @@
 # Generate fake data to populate database
-
+import random
+import sys
 from enum import Enum
 
 MIN_HER0 = 101
@@ -17,6 +18,9 @@ MAX_VILLAIN = 110
 MIN_HENCHMEN = 101
 MAX_HENCHMEN = 104
 
+USER_ID = 0
+USER_NAME = "system_automation"
+
 
 class Outcomes(Enum):
     WIN = "WIN"
@@ -24,8 +28,12 @@ class Outcomes(Enum):
     LOSS = "LOSS"
     INC = "INCOMPLETE"
 
-    def __init__(self, repr):
-        self.db_value = repr
+    def __init__(self, represent):
+        self.db_value = represent
+
+    @classmethod
+    def as_list(cls):
+        return list(cls)
 
 
 class Players(Enum):
@@ -36,10 +44,71 @@ class Players(Enum):
     FOUR = "FOUR"
     FIVE = "FIVE"
 
+    def __init__(self, represent):
+        self.db_value = represent
 
-def generate_db(num_plays):
-    pass
+    @classmethod
+    def as_list(cls):
+        return list(cls)
+
+
+def generate_db(starting_id, num_plays, out_file):
+    generate_user(out_file)
+    plays = generate_plays(starting_id, num_plays, out_file)
+    generate_heroes(plays, out_file)
+
+
+def generate_user(out_file):
+    out_file.write("""INSERT INTO \"user\" (id, user_name)
+        VALUES (%d, '%s')
+        ON CONFLICT DO NOTHING;\n\n""" % (USER_ID, USER_NAME))
+
+
+def generate_plays(starting_id, num_plays, out_file):
+    plays = {}
+
+    # print start of query
+    out_file.write("INSERT INTO play (id, player_id, outcome, mastermind_id, scheme_id, players) VALUES \n")
+
+    for id in range(starting_id, starting_id + num_plays):
+        if id > starting_id:
+            out_file.write(",\n")
+
+        outcome = random.choice(Outcomes.as_list()).db_value
+        mastermind = random.randint(MIN_MASTERMIND, MAX_MASTERMIND)
+        scheme = random.randint(MIN_SCHEME, MAX_SCHEME)
+        players = random.choice(Players.as_list()).db_value
+
+        value_format = "\t(%d, %d, '%s', %d, %d, '%s')"
+        out_file.write(value_format % (id, USER_ID, outcome, mastermind, scheme, players))
+        plays[id] = {"scheme": scheme, "players": players}
+
+    out_file.write(";\n\n")
+
+    return plays
+
+
+def generate_heroes(plays, out_file):
+    out_file.write("INSERT INTO play_hero (play_id, hero_id) VALUES\n")
+    first = True
+    for play_id, play in plays.items():
+        players = play["players"]
+        if players in [Players.SOLO.db_value, Players.ADVANCED.db_value]:
+            num_heroes = 3
+        elif players in [Players.TWO.db_value, Players.THREE.db_value, Players.FOUR.db_value]:
+            num_heroes = 5
+        elif players is Players.FIVE.db_value:
+            num_heroes = 6
+
+        for hero_id in random.choices(range(MIN_HER0, MAX_HERO + 1), k=num_heroes):
+            if not first:
+                out_file.write(",\n")
+            else:
+                first = False
+            out_file.write("\t(%d, %d)" % (play_id, hero_id))
+
+    out_file.write(";\n\n")
 
 
 if __name__ == '__main__':
-    generate_db(100)
+    generate_db(5, 10, sys.stdout)
