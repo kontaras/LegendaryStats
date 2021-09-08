3661bfd94621b042b0f09cac6b3c1a224f0762a5
Konstantin Tarashchanskiy <2186793+kontaras@users.noreply.github.com>
2021-09-07 23:58:57 -0400

    Add comments to Verifier

--- rules/src/commonMain/kotlin/verifier/Verifier.kt
+++ rules/src/commonMain/kotlin/verifier/Verifier.kt
@@ -3,16 +3,29 @@ package games.lmdbg.rules.verifier
 import games.lmdbg.rules.model.Play
 import games.lmdbg.rules.model.PlayerCount
 
+/**
+ * Check if a given play follows all game setup rules
+ *
+ * @param play The play to verify
+ * @return A list of issues with this play. Will be empty if there are no issues.
+ */
 fun verify(play: Play): List<PrintableError> {
     val errors = mutableListOf<PrintableError>()
     val setCounts = getPlayerCountRules(play.players)
 
-    errors.addAll(checkSetSizes(play, setCounts))
+    errors.addAll(checkCardSetSizes(play, setCounts))
 
     return errors
 }
 
-fun checkSetSizes(play: Play, setCounts: SetCounts): List<PrintableError> {
+/**
+ * Check if each of the card sets from the play has the correct amount of items.
+ *
+ * @param play The play to check
+ * @param setCounts The expected number of items in each set
+ * @return A list containing [WrongSetCount]s for sets that have the wrong number of items
+ */
+fun checkCardSetSizes(play: Play, setCounts: SetCounts): List<PrintableError> {
     val errors = mutableListOf<PrintableError>()
     if (play.heroes.size != setCounts.heroes) {
         errors.add(WrongSetCount("hero", setCounts.heroes, play.heroes.size))
@@ -29,18 +42,32 @@ fun checkSetSizes(play: Play, setCounts: SetCounts): List<PrintableError> {
     return errors
 }
 
+/**
+ * Check if each card set index in a play is a valid card set id.
+ *
+ * @param play The play to check
+ * @return [InvalidCardSet] for each invalid card set id
+ */
 fun checkValuesInRange(play: Play): List<PrintableError> {
     val errors = mutableListOf<PrintableError>()
 
-    for(hero in play.heroes) {
+    for (hero in play.heroes) {
         if (!checkValidHero(hero)) {
             errors.add(InvalidCardSet("hero", hero))
         }
     }
 
+    //TODO Check other sets
+
     return errors
 }
 
+/**
+ * Check if a given hero id is a valid one
+ *
+ * @param hero Hero ID to check
+ * @return true if there is a set that contains that id, false otherwise
+ */
 private fun checkValidHero(hero: Int): Boolean {
     for (plugin in plugins) {
         if (hero in plugin.heroesRange) {
@@ -51,6 +78,12 @@ private fun checkValidHero(hero: Int): Boolean {
     return false
 }
 
+/**
+ * Get the standard number of card sets of each type that are expected for a given number of players
+ *
+ * @param playerCount The number of players in a play
+ * @return The default number of card sets for the player count
+ */
 fun getPlayerCountRules(playerCount: PlayerCount): SetCounts {
     return when (playerCount) {
         PlayerCount.SOLO -> SetCounts(3, 1, 1)
@@ -62,5 +95,12 @@ fun getPlayerCountRules(playerCount: PlayerCount): SetCounts {
     }
 }
 
+/**
+ * A mutable structure to store the number of card sets of each type that the play should have
+ *
+ * @property heroes The number of hero sets that are expected for the play
+ * @property villains The number of villain sets that are expected for the play
+ * @property henchmen The number of henchmen sets that are expected for the play
+ */
 data class SetCounts(var heroes: Int, var villains: Int, var henchmen: Int)
 
