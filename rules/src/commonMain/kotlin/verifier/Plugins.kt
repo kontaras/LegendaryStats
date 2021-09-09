905339122b56ca4c1c0f40e723a213b1a0093be2
Konstantin Tarashchanskiy <2186793+kontaras@users.noreply.github.com>
2021-09-08 21:16:52 -0400

    Issue 108: Add comments to Errors and Plugins

--- rules/src/commonMain/kotlin/verifier/Plugins.kt
+++ rules/src/commonMain/kotlin/verifier/Plugins.kt
@@ -1,7 +1,11 @@
 package games.lmdbg.rules.verifier
 
+/** A list of all the release rules engine plugins */
 val plugins: MutableSet<ReleaseRulesPlugin> = mutableSetOf()
 
+/** Data about each release that the rules engine needs to validate sets including that release */
 interface ReleaseRulesPlugin {
+
+    /** The range of valid ID numbers for hero sets */
     val heroesRange: IntRange
 }
