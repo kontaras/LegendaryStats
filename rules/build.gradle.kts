9bbff82a308d6726026bdc0f73b03f159de9427c
Konstantin Tarashchanskiy <2186793+kontaras@users.noreply.github.com>
2021-09-16 13:10:06 -0400

    Added JaCoCo report target

--- rules/build.gradle.kts
+++ rules/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     kotlin("multiplatform") version "1.5.31"
     id("org.sonarqube") version "3.3"
+    id("java-library")
+    jacoco
 }
 
 group = "games.lmdbg.rules"
@@ -41,10 +43,39 @@ kotlin {
         val jsTest by getting
     }
 }
+
 sonarqube {
     properties {
         property("sonar.projectKey", "knary_LegendaryStats")
-        property( "sonar.organization", "kon")
-        property( "sonar.host.url", "https://sonarcloud.io")
+        property("sonar.organization", "kon")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
+
+tasks.withType<JacocoReport> {
+    dependsOn("test")
+    dependsOn("jvmTest")
+    group = "Reporting"
+    description = "Generate Jacoco coverage reports."
+    val coverageSourceDirs = arrayOf(
+        "commonMain/src",
+        "jvmMain/src"
+    )
+    val classFiles = File("${buildDir}/classes/kotlin/jvm/")
+        .walkBottomUp()
+        .toSet()
+    classDirectories.setFrom(classFiles)
+    sourceDirectories.setFrom(files(coverageSourceDirs))
+    additionalSourceDirs.setFrom(files(coverageSourceDirs))
+
+    executionData
+        .setFrom(files("${buildDir}/jacoco/jvmTest.exec"))
+
+    reports {
+        xml.isEnabled = true
+        csv.isEnabled = false
+        html.isEnabled = true
+        html.destination =
+            File("${buildDir}/jacoco-reports/html")
     }
 }
