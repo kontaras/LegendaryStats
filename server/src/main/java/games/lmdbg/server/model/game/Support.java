5eba881dd8485ee8c747a08437a0e4df6799ac15
Konstantin Tarashchanskiy <2186793+kontaras@users.noreply.github.com>
2021-09-30 13:03:06 -0400

    Issue 138: Added support decks to db schema and spring data

--- server/src/main/java/games/lmdbg/server/model/game/Support.java
+++ server/src/main/java/games/lmdbg/server/model/game/Support.java
@@ -1,28 +1,20 @@
 package games.lmdbg.server.model.game;
 
-import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import org.hibernate.annotations.Cache;
-import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import games.lmdbg.server.model.Play;
 
-/**
- * A hero card set
- */
-@Entity
-@Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
-public class Hero extends CardSet {
+public class Support extends CardSet {
 	/**
-	 * Plays of the game which include this hero.
+	 * Plays of the game which include this villain group.
 	 */
 	@ManyToMany
 	@JsonIgnore 
-	@JoinTable(name = "play_hero", inverseJoinColumns = { @JoinColumn(name = "play_id") })
+	@JoinTable(name = "play_support", inverseJoinColumns = { @JoinColumn(name = "play_id") })
 	private java.util.Set<Play> plays;
 
 	/**
@@ -31,4 +23,5 @@ public class Hero extends CardSet {
 	public java.util.Set<Play> getPlays() {
 		return plays;
 	}
+
 }
