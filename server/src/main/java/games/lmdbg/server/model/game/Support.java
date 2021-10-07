958cbba84ca30ec7c3d801de810764325e710748
Konstantin Tarashchanskiy <2186793+kontaras@users.noreply.github.com>
2021-10-07 13:11:20 -0400

    Added Starter decks to schema and ORM, added tests to Starter, StarterPlay, and Support.

--- server/src/main/java/games/lmdbg/server/model/game/Support.java
+++ server/src/main/java/games/lmdbg/server/model/game/Support.java
@@ -1,5 +1,6 @@
 package games.lmdbg.server.model.game;
 
+import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
@@ -8,12 +9,16 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import games.lmdbg.server.model.Play;
 
+/**
+ * A support deck that could be included as always available for a play
+ */
+@Entity
 public class Support extends CardSet {
 	/**
-	 * Plays of the game which include this villain group.
+	 * Plays of the game which include this support deck.
 	 */
 	@ManyToMany
-	@JsonIgnore 
+	@JsonIgnore
 	@JoinTable(name = "play_support", inverseJoinColumns = { @JoinColumn(name = "play_id") })
 	private java.util.Set<Play> plays;
 
