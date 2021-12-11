0253b9d7bfbd470b7dea799cad9c9159331ff046
Konstantin Tarashchanskiy <2186793+kontaras@users.noreply.github.com>
2021-12-11 17:09:23 -0500

    Added tests to ScriptsController.java and ThymeleafConfig.java

--- server/src/main/java/games/lmdbg/server/view/ScriptsController.java
+++ server/src/main/java/games/lmdbg/server/view/ScriptsController.java
@@ -1,12 +1,9 @@
 package games.lmdbg.server.view;
 
-import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import games.lmdbg.server.model.game.Henchman;
 import games.lmdbg.server.model.game.Villain;
 import games.lmdbg.server.service.CardCache;
@@ -21,8 +18,11 @@ public class ScriptsController {
 	@Autowired
 	private CardCache<Henchman> henchmen;
 
-	@GetMapping("/autoscripts/cardLookup")
-	public String getVillains(Model model) {
+	/**
+	 * Generate the JavaScript card set lookup script
+	 */
+	@GetMapping(value = "/autoscripts/cardLookup", produces = "text/javascript")
+	public String cardDeref(Model model) {
 		model.addAttribute("villainsMap",  villains.getAllById());
 		model.addAttribute("henchmenMap",  henchmen.getAllById());
 		return "scripts/cardSet";
--- server/src/main/java/games/lmdbg/server/view/ThymeleafConfig.java
+++ server/src/main/java/games/lmdbg/server/view/ThymeleafConfig.java
@@ -4,7 +4,6 @@ import java.util.Collections;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
