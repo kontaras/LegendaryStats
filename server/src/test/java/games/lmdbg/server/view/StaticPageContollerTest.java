f459ba1a2552b7c7156eddf3d40c2779658ef82e
Konstantin Tarashchanskiy <2186793+kontaras@users.noreply.github.com>
2022-01-10 12:56:32 -0500

    Added a FAQ page.

---
+++ server/src/test/java/games/lmdbg/server/view/StaticPageContollerTest.java
@@ -0,0 +1,22 @@
+package games.lmdbg.server.view;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for {@link StaticPageContoller}
+ */
+public class StaticPageContollerTest {
+
+	/** Instance under test. */
+	private StaticPageContoller testInstance;
+
+	/**
+	 * Test {@link StaticPageContoller#faqPage()}
+	 */
+	@Test
+	void testFaqPage() {
+		testInstance = new StaticPageContoller();
+		Assertions.assertEquals("faq", testInstance.faqPage());
+	}
+}
