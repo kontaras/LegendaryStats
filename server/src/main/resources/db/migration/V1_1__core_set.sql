4fc2074619f0f5a4c3747a1c129d0c5c50bd42f4
Konstantin Naryshkin <knary@users.noreply.github.com>
2022-01-13 21:55:54 -0500

    Add game board to db schema

--- server/src/main/resources/db/migration/V1_1__core_set.sql
+++ server/src/main/resources/db/migration/V1_1__core_set.sql
@@ -70,3 +70,6 @@ INSERT INTO support (release_id, id, marvel_name, dxp_name, mcu_name) VALUES
 	
 INSERT INTO starter (release_id, id, marvel_name, dxp_name, mcu_name) VALUES
 	(1, 101, 'S.H.I.E.L.D.', 'Fixers', 'S.H.I.E.L.D.');
+
+INSERT INTO board (release_id, id, marvel_name, dxp_name, mcu_name) VALUES
+	(1, 101, 'Heroic.', 'Heroic', 'Heroic');
