466729127b52aef9e870200789f60699fe11bd99
Konstantin Tarashchanskiy <2186793+kontaras@users.noreply.github.com>
2022-05-04 19:18:49 +0000

    Switch to kover for Kotlin test coverage

--- rules/build.gradle.kts
+++ rules/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     kotlin("plugin.serialization") version "1.6.21"
     id("org.sonarqube") version "3.3"
     id("java-library")
-    jacoco
+    id("org.jetbrains.kotlinx.kover") version "0.5.0"
 }
 
 group = "games.lmdbg.rules"
@@ -59,33 +59,5 @@ sonarqube {
         property("sonar.projectKey", "knary_LegendaryStats")
         property("sonar.organization", "kon")
         property("sonar.host.url", "https://sonarcloud.io")
-    }
-}
-
-tasks.withType<JacocoReport> {
-    dependsOn("test")
-    dependsOn("jvmTest")
-    group = "Reporting"
-    description = "Generate Jacoco coverage reports."
-    val coverageSourceDirs = arrayOf(
-        "commonMain/src",
-        "jvmMain/src"
-    )
-    val classFiles = File("${buildDir}/classes/kotlin/jvm/")
-        .walkBottomUp()
-        .toSet()
-    classDirectories.setFrom(classFiles)
-    sourceDirectories.setFrom(files(coverageSourceDirs))
-    additionalSourceDirs.setFrom(files(coverageSourceDirs))
-
-    executionData
-        .setFrom(files("${buildDir}/jacoco/jvmTest.exec"))
-
-    reports {
-        xml.isEnabled = true
-        csv.isEnabled = false
-        html.isEnabled = true
-        html.destination =
-            File("${buildDir}/jacoco-reports/html")
     }
 }
